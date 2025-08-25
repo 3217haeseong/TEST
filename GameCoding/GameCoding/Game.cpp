bdbb433b544b8f018dbb9b9f0ab500156ecd2ffd
haeseong <32110495+3217haeseong@users.noreply.github.com>
2025-08-25 18:58:24 +0900

    Add files via upload

--- GameCoding/GameCoding/Game.cpp
+++ GameCoding/GameCoding/Game.cpp
@@ -14,6 +14,12 @@ void Game::Init(HWND hwnd)
 	_hwnd = hwnd;
 	_width = GWinSizeX;
 	_height = GWinSizeY;
+
+	CreateDeviceAndSwapChain();
+	CreateRenderTargetView();
+	SetViewport();
+
+	CreateGeometry();
 }
 
 void Game::Update()
@@ -22,4 +28,136 @@ void Game::Update()
 
 void Game::Render()
 {
+	RenderBegin();
+
+	RenderEnd();
+}
+
+void Game::RenderBegin()
+{
+	_deviceContext->OMSetRenderTargets(1, _renderTargetView.GetAddressOf(),nullptr);
+	_deviceContext->ClearRenderTargetView(_renderTargetView.Get(), _clearColor);
+	_deviceContext->RSSetViewports(1, &_viewport);
+
+}
+
+void Game::RenderEnd()
+{
+	HRESULT hr = _swapChain->Present(1, 0);
+	CHECK(hr);
+}
+
+void Game::CreateDeviceAndSwapChain()
+{
+	DXGI_SWAP_CHAIN_DESC desc;
+	ZeroMemory(&desc, sizeof(desc));
+	{
+		desc.BufferDesc.Width = _width;
+		desc.BufferDesc.Height = _height;
+		desc.BufferDesc.RefreshRate.Numerator = 60;
+		desc.BufferDesc.RefreshRate.Denominator = 1;
+		desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		desc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+		desc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+		desc.SampleDesc.Count = 1;
+		desc.SampleDesc.Quality = 0;
+		desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		desc.BufferCount = 1;
+		desc.OutputWindow = _hwnd;
+		desc.Windowed = true;
+		desc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	}
+
+	HRESULT hr = ::D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		0,
+		nullptr,
+		0,
+		D3D11_SDK_VERSION,
+		&desc,
+		_swapChain.GetAddressOf(),
+		_device.GetAddressOf(),
+		nullptr,
+		_deviceContext.GetAddressOf()
+	);
+
+	CHECK(hr);
+}
+
+void Game::CreateRenderTargetView()
+{
+	HRESULT hr;
+
+	ComPtr<ID3D11Texture2D> backBuffer = nullptr;
+	hr = _swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)backBuffer.GetAddressOf());
+	CHECK(hr);
+
+	_device->CreateRenderTargetView(backBuffer.Get(), nullptr, _renderTargetView.GetAddressOf());
+
+}
+
+void Game::SetViewport()
+{
+	_viewport.TopLeftX = 0.f;
+	_viewport.TopLeftY = 0.f;
+	_viewport.Width = static_cast<float>(_width);
+	_viewport.Height = static_cast<float>(_height);
+	_viewport.MinDepth = 0.f;
+	_viewport.MaxDepth = 1.f;
+}
+
+void Game::CreateGeometry()
+{
+	// VertexData
+	{
+		_vertices.resize(3);
+
+		_vertices[0].position = Vec3(-0.5f, -0.5f, 0.f);
+		_vertices[0].color = Color(1.f, 0.f, 0.f, 1.f);
+	
+		_vertices[1].position = Vec3(0.f, 0.5f, 0.f);
+		_vertices[1].color = Color(1.f, 0.f, 0.f, 1.f);
+
+		_vertices[2].position = Vec3(0.5f, -0.5f, 0.f);
+		_vertices[2].color = Color(1.f, 0.f, 0.f, 1.f);
+	}
+
+	// VertexBuffer
+	{
+		D3D11_BUFFER_DESC desc;
+		ZeroMemory(&desc, sizeof(desc));
+		// D3D11_USAGE_IMMUTABLE = GPU만 읽을 수 있는 데이터. 
+		desc.Usage = D3D11_USAGE_IMMUTABLE;
+		desc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+		desc.ByteWidth = (uint32)sizeof(Vertex) + _vertices.size();
+
+		D3D11_SUBRESOURCE_DATA data;
+		ZeroMemory(&data, sizeof(data));
+		data.pSysMem = _vertices.data();
+
+		_device->CreateBuffer(&desc, &data, _vertexBuffer.GetAddressOf());
+	}
+	
+}
+
+void Game::CreateInputLayout()
+{
+	//Vertex가 어떤 식으로 이루어져 있는지 설명
+	D3D11_INPUT_ELEMENT_DESC layout[] =
+	{
+		{"POSITION",0,DXGI_FORMAT_R32G32B32_FLOAT,0,0, D3D11_INPUT_PER_VERTEX_DATA,0},
+		{"COLOR",0,DXGI_FORMAT_R32G32B32A32_FLOAT,0,12, D3D11_INPUT_PER_VERTEX_DATA,0},
+	};
+
+	const int32 count = sizeof(layout) / sizeof(D3D11_INPUT_ELEMENT_DESC);
+	//_device->CreateInputLayout(layout, count,);
+}
+
+void Game::LoadShaderFromFile(const wstring& path, const string& name, const string& version, ComPtr<ID3DBlob>& blob)
+{
+	::D3DCompileFromFile(
+
+	)
 }
--- GameCoding/GameCoding/Game.h
+++ GameCoding/GameCoding/Game.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 class Game
 {
 public:
@@ -11,11 +12,58 @@ public:
 	void Render();
 
 private:
+	void RenderBegin();
+	void RenderEnd();
+
+private:
+	// 장치와 스왑체인 생성
+	void CreateDeviceAndSwapChain();
+	// 렌더링될 곳으로 후면버퍼 지정
+	void CreateRenderTargetView();
+
+	// 뷰포트의 크기 지정을 위한 함수.
+	void SetViewport();
+
+private:
+	// 정점 데이터를 만들어 줌.
+	void CreateGeometry();
+	// 파이프라인 과정에서 건네주는 데이터에 대한 레이아웃을 알려주는 함수.
+	void CreateInputLayout();
+
+	void LoadShaderFromFile(const wstring& path, const string& name, const string& version, ComPtr<ID3DBlob>& blob);
+
+
+
+private:
 	HWND _hwnd;
 	uint32 _width = 0;
 	uint32 _height = 0;
 
 private:
 	// DX
+	ComPtr<ID3D11Device> _device;
+	ComPtr<ID3D11DeviceContext> _deviceContext;
+	ComPtr<IDXGISwapChain> _swapChain = nullptr;
+
+	// RTV
+	ComPtr<ID3D11RenderTargetView> _renderTargetView;
+
+	// Misc
+	D3D11_VIEWPORT _viewport = { 0 };
+	float _clearColor[4] = { 0.5f,0.5f,0.5f,0.5f };
+
+private:
+	// Geometry
+	vector<Vertex> _vertices;
+	// GPU의 VRAM에 데이터를 넣기 위한 변수
+	ComPtr<ID3D11Buffer> _vertexBuffer = nullptr;
+	// VS
+	ComPtr<ID3D11VertexShader> _vertexShader = nullptr;
+	ComPtr<ID3DBlob> _vsBlob = nullptr;
+	
+	// PS
+	ComPtr<ID3D11PixelShader> _pixelShader = nullptr;
+	ComPtr<ID3DBlob> _osBlob = nullptr;
+
 };
 
